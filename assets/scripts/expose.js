8a53a61c5cc91116ce3b3f10fd3296571efaf854
P7chang <p7chang@ucsd.edu>
2024-05-05 18:28:50 -0700

    commit 1: image  select

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -4,4 +4,24 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+  const selectorHorns = document.getElementById("horn-select"); //selector element
+  const selectedImage = document.querySelector("img"); //image representing the selected horn
+  console.log(selectedImage); //print in console for debug
+  selectorHorns.addEventListener('change', (event) => { //call function when selector changed by user
+    selectedImage.src = selectHornImage(selectorHorns.value)
+  })
+
+}
+
+//function that return image source based off selected selector value
+function selectHornImage(selectValue) {
+  if (selectValue == "air-horn") {
+    return "assets/images/air-horn.svg";
+  }
+  else if (selectValue == "car-horn") {
+    return "assets/images/car-horn.svg";
+  }
+  else {
+    return "assets/images/party-horn.svg";
+  }
 }
