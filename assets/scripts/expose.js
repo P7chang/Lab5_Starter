73bd2cbaec30686002fba1beaa52e44324048f87
P7chang <p7chang@ucsd.edu>
2024-05-05 18:38:43 -0700

    Commit 2: Audio select

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -6,9 +6,16 @@ function init() {
   // TODO
   const selectorHorns = document.getElementById("horn-select"); //selector element
   const selectedImage = document.querySelector("img"); //image representing the selected horn
-  console.log(selectedImage); //print in console for debug
+  const selectedAudio = document.querySelector(".hidden"); //audio element
+  //print in console for debug
+  console.log(selectedImage);
+  console.log(selectedAudio);
+
   selectorHorns.addEventListener('change', (event) => { //call function when selector changed by user
-    selectedImage.src = selectHornImage(selectorHorns.value)
+    let selectedHorn = selectorHorns.value;
+    selectedImage.src = selectHornImage(selectedHorn);
+    selectedAudio.src = selectAudio(selectedHorn);
+    //console.log(selectedAudio); //uncomment to debug selected audio file
   })
 
 }
@@ -23,5 +30,17 @@ function selectHornImage(selectValue) {
   }
   else {
     return "assets/images/party-horn.svg";
+  }
+}
+//function that return audio source based off selected selector value
+function selectAudio(selectValue) {
+  if (selectValue == "air-horn") {
+    return "assets/audio/air-horn.mp3";
+  }
+  else if (selectValue == "car-horn") {
+    return "assets/audio/car-horn.mp3";
+  }
+  else {
+    return "assets/audio/party-horn.mp3";
   }
 }
