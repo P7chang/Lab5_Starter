7e9c4f33f23ce2b467a41db2edc49315fbc98f96
P7chang <p7chang@ucsd.edu>
2024-05-05 19:04:55 -0700

    Commit 3: Volume Icon change and audio volume set

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -7,15 +7,29 @@ function init() {
   const selectorHorns = document.getElementById("horn-select"); //selector element
   const selectedImage = document.querySelector("img"); //image representing the selected horn
   const selectedAudio = document.querySelector(".hidden"); //audio element
+  const volumeIcon = document.querySelector("img[alt = 'Volume level 2']"); //volume icon
+  const volumeSelector = document.querySelector("#volume"); //volume slider
   //print in console for debug
   console.log(selectedImage);
   console.log(selectedAudio);
+  console.log(volumeIcon);
+  console.log(volumeSelector);
+  console.log(volumeSelector.value);
 
-  selectorHorns.addEventListener('change', (event) => { //call function when selector changed by user
+  //call functions when horn selector changed by user
+  selectorHorns.addEventListener('change', (event) => {
     let selectedHorn = selectorHorns.value;
     selectedImage.src = selectHornImage(selectedHorn);
     selectedAudio.src = selectAudio(selectedHorn);
     //console.log(selectedAudio); //uncomment to debug selected audio file
+  })
+
+  //call functions when volume slider changed by user
+  volumeSelector.addEventListener('change', (event) => {
+    let selectedVol = volumeSelector.value;
+    volumeIcon.src = selectVolume(selectedVol);
+    selectedAudio.volume = selectedVol/100;
+    //console.log(selectedAudio.volume); //uncomment to debug audio volue setting
   })
 
 }
@@ -42,5 +56,20 @@ function selectAudio(selectValue) {
   }
   else {
     return "assets/audio/party-horn.mp3";
+  }
+}
+//function that return icon source based off selected audio level
+function selectVolume(selectValue) {
+  if (selectValue == 0) {
+    return "assets/icons/volume-level-0.svg";
+  }
+  else if (selectValue < 33) {
+    return "assets/icons/volume-level-1.svg";
+  }
+  else if (selectValue < 67) {
+    return "assets/icons/volume-level-2.svg";
+  }
+  else {
+    return "assets/icons/volume-level-3.svg";
   }
 }
